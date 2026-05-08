b5ed83e3ce076aa7399842df37485ba46f01d026
Agentfcb <babyshark290907@gmail.com>
2026-05-08 23:54:27 +0300

    Fix vite config for local and github

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Ma-bel/',  
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Если запускаем у себя (serve) - корень '/', если собираем для GitHub (build) - папка '/Ma-bel/'
+    base: command === 'serve' ? '/' : '/Ma-bel/',
+  }
 })
